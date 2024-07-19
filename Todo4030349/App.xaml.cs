29a60a5d685e53fe6b656bb923849d13eedcc829
CHATO LOKO <colochocristian72@gmail.com>
2024-07-19 17:06:47 -0600

    Agregar archivos de proyecto.

---
+++ Todo4030349/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo4030349
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Todo4030349/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Todo4030349.Views;
+namespace Todo4030349
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+        }
+    }
+}
